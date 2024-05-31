095d222920d3130e6e8d5a2f251f2687bed7d156
Sebastian Coroian <79795016+scoroian@users.noreply.github.com>
2024-05-31 20:34:16 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,10 @@ module.exports = {
   'extends': 'eslint:recommended',
   'parserOptions': {
     'sourceType': 'module'
-  },
+  },,
+  'globals': {
+    'gtag': 'readonly'
+  }
   'rules': {
     'no-cond-assign': [
       'error',
